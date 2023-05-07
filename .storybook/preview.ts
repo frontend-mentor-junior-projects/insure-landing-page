5024429a7d7e1998d65a3c9d98c2e5b2b067629e
likelytwitchdollop <cynthiamahofa@yahoo.com>
2023-05-08 00:09:16 +0200

    chore: setup storybook viewports and font

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,33 @@
 import type { Preview } from '@storybook/react'
 import 'tailwindcss/tailwind.css'
 
+const CUSTOM_VIEWPORTS = {
+	mobile375: {
+		name: 'Mobile 375',
+		styles: {
+			width: '375px',
+			height: '600px',
+		},
+		type: 'mobile',
+	},
+	tablet768: {
+		name: 'Tablet 768',
+		styles: {
+			width: '768px',
+			height: '1024px',
+		},
+		type: 'mobile',
+	},
+	desktop1440: {
+		name: 'Desktop 1440',
+		styles: {
+			width: '1440px',
+			height: '1024px',
+		},
+		type: 'desktop',
+	},
+}
+
 const preview: Preview = {
 	parameters: {
 		actions: { argTypesRegex: '^on[A-Z].*' },
@@ -15,6 +42,9 @@ const preview: Preview = {
 				a.id === b.id
 					? 0
 					: a.id.localeCompare(b.id, undefined, { numeric: true }),
+		},
+		viewport: {
+			viewports: CUSTOM_VIEWPORTS,
 		},
 	},
 }
