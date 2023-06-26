db4fd9864405e0c2847c9a7bbc708eacc3e18da6
likelytwitchdollop <cynthiamahofa@yahoo.com>
2023-06-26 15:49:01 +0200

    feat: create Home page

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Button from './Button'
+import Footer from './Footer'
+import Header from './Header'
+import Logo from './Logo'
+
+export { Button, Footer, Header, Logo }
---
+++ src/pages/HomePage/components/Card/Card.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from '@storybook/react'
+import utils from 'utils'
+
+import Card from '.'
+
+const meta: Meta<typeof Card> = {
+	title: 'Components/Card',
+	component: Card,
+	args: {
+		content: utils.SERVICES[0],
+	},
+}
+
+export default meta
+type Story = StoryObj<typeof Card>
+
+export const Default: Story = {}
